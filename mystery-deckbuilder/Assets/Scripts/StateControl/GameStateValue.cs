97ddaf99c0808597a39b02bd8d67b16b31527195
Gabriel LePoudre <gabelepoudre@gmail.com>
2023-01-28 15:02:42 -0600

    documentation

--- mystery-deckbuilder/Assets/Scripts/StateControl/GameStateValue.cs
+++ mystery-deckbuilder/Assets/Scripts/StateControl/GameStateValue.cs
@@ -1,5 +1,15 @@
+/*
+ * author(s): Gabriel LePoudre
+ * 
+ * This script stores the GameStateValue class, which exists to store values pertaining to game state
+ */
+
 using System.Collections.Generic;
 
+/*
+ * The GameStateValue class is a wrapper for any value that includes a "pub-sub" architecture
+ * Use this class to store values within the GameState.cs file and allow subscribers to be notified on change
+ */
 public class GameStateValue<T> {
     private T __value;
     private List<GameStateValueSubscriber<T>> __subscribers;
@@ -10,10 +20,15 @@ public class GameStateValue<T> {
 
     public T GetValue() { return this.__value; }
 
+    /* Adds a given subscriber to the subscriber list */
     public void Subscribe(GameStateValueSubscriber<T> subscriberObject) {
         this.__subscribers.Add(subscriberObject);
     }
 
+    /*
+     * Changes the value to another value of the same type
+     * NOTE: changes notifies all subscribers of the change, if any exist
+     */
     public void ChangeValue(T newValue) {
         this.__value = newValue;
 
--- mystery-deckbuilder/Assets/Scripts/StateControl/GameStateValueSubscriber.cs
+++ mystery-deckbuilder/Assets/Scripts/StateControl/GameStateValueSubscriber.cs
@@ -1,8 +1,15 @@
-public abstract class GameStateValueSubscriber<T>
-{
-    public GameStateValueSubscriber(GameStateValue<T> gameStateValue)
-    {
+/*
+ * author(s): Gabriel LePoudre
+ * 
+ * This script contains the GameStateValueSubscriber class for subscribing to a GameStateValue
+ */
+
+
+public abstract class GameStateValueSubscriber<T> {
+    public GameStateValueSubscriber(GameStateValue<T> gameStateValue) {
         gameStateValue.Subscribe(this);
     }
+
+    /* This is what is extended when you make one of these classes. What do you want it to do when it changes? */
     public abstract void OnValueChange(T newValue);
 }
