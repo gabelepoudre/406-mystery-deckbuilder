1efd025cc60b94d47bef6a747b2425e10a2564be
Gabriel LePoudre <gabelepoudre@gmail.com>
2023-02-02 19:30:49 -0600

    OnMouseEnter and some style stuff (my bad!)

--- mystery-deckbuilder/Assets/Scripts/HoverClick/HoverClick.cs
+++ mystery-deckbuilder/Assets/Scripts/HoverClick/HoverClick.cs
@@ -1,19 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class HoverClick : MonoBehaviour
 {
-
-    public virtual void OnMouseOver(){
-
-    }
-
-    public virtual void OnMouseExit(){
+    public virtual void OnMouseEnter()
+    {
 
     }
 
-    public virtual void OnMouseDown(){
+    public virtual void OnMouseOver()
+    {
+
+    }
+
+    public virtual void OnMouseExit()
+    {
+
+    }
+
+    public virtual void OnMouseDown()
+    {
         
     }
 
--- mystery-deckbuilder/Assets/Scripts/HoverClick/HoverClickExample.cs
+++ mystery-deckbuilder/Assets/Scripts/HoverClick/HoverClickExample.cs
@@ -1,25 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HoverClickExample : HoverClick
 {
 
-    
+    public override void OnMouseEnter()
+    {
+        base.OnMouseEnter();
+        Debug.Log("Mouse Enter Called");
+    }
 
-    public override void OnMouseOver(){
+    public override void OnMouseOver()
+    {
         base.OnMouseOver();
         Debug.Log("Mouse Hover Called");
 
     }
 
-    public override void OnMouseExit(){
+    public override void OnMouseExit()
+    {
         base.OnMouseExit();
         Debug.Log("Mouse Exit Called");
 
     }
 
-    public override void OnMouseDown(){
+    public override void OnMouseDown()
+    {
         base.OnMouseDown();
         Debug.Log("Mouse clicked");
     }
