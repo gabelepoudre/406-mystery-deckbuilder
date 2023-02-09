97580e0e778e8370d277a840d8d65a3cbe9479df
DUCSalatin <Salatin@live.cn>
2023-02-08 18:24:21 -0600

    Update
    Update few writing things, no major change and tested.

--- mystery-deckbuilder/Assets/Scripts/MainMenu/MainMenu.cs
+++ mystery-deckbuilder/Assets/Scripts/MainMenu/MainMenu.cs
@@ -7,12 +7,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void play() 
+    public void Play() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
-    public void quit()
+    public void Quit()
     {
         Debug.Log("Quit");
         Application.Quit();
