45526d89b34c25c17f9fde2f3c5d04d8c7912811
DUCSalatin <Salatin@live.cn>
2023-03-08 17:23:29 -0600

    Mini_Map_update
    missing "hide unknown label" function
    all button not in "main street" scene not working

---
+++ mystery-deckbuilder/Assets/Scripts/MapOverlay/Label.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Label : MonoBehaviour
+{
+    public bool LabelDisplay = false;
+
+    public void MoveToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void display()
+    {
+        LabelDisplay = true;
+    }
+
+    public void hide()
+    {
+        LabelDisplay = false;
+    }
+
+
+    
+}
