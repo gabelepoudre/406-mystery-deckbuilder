9c254b238fdab4de23d642d3008875f678ac89ef
Gabriel LePoudre <gabelepoudre@gmail.com>
2023-03-12 12:23:35 -0600

    UI hit detection improved

---
+++ mystery-deckbuilder/Assets/Scripts/World UI/AlphaHitDetection.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlphaHitDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
