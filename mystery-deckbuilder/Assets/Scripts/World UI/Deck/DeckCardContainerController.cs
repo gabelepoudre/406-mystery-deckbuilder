e0a443eba2dd7901eaf510d59d52e50efffaa9fb
Gabriel LePoudre <gabelepoudre@gmail.com>
2023-03-12 17:59:30 -0600

    showing a card

--- mystery-deckbuilder/Assets/Scripts/World UI/Deck/DeckCardContainerController.cs
+++ mystery-deckbuilder/Assets/Scripts/World UI/Deck/DeckCardContainerController.cs
@@ -5,5 +5,19 @@ using UnityEngine;
 public class DeckCardContainerController : MonoBehaviour
 {
     public Transform spawn;
+    public DeckCardQuantityController quantityController;
 
+    private Card _currentCard;
+    private GameObject _cardPrefabInstance;
+    private DeckUIController _parentController;
+
+    public void Start()
+    {
+        _parentController = GetComponentInParent<DeckUIController>();
+    }
+
+    public void SetCard(Card card)
+    {
+        _currentCard = card;
+    }
 }
--- mystery-deckbuilder/Assets/Scripts/World UI/Deck/DeckUIController.cs
+++ mystery-deckbuilder/Assets/Scripts/World UI/Deck/DeckUIController.cs
@@ -14,15 +14,50 @@ public class DeckUIController : MonoBehaviour
     public GameObject greyCardNoEncounter;
 
     private List<Text> _deckQuantities = new();
-    private List<GameObject> _deckSpawns = new();
+    private List<DeckCardContainerController> _deckContainerControllers = new();
 
     public void Start()
     {
         foreach(GameObject deckContainer in deckContainers)
         {
             _deckQuantities.Add(deckContainer.GetComponentInChildren<Text>());
+            _deckContainerControllers.Add(deckContainer.GetComponent<DeckCardContainerController>());
 
         }
+
+        Card card = (Card)Cards.CreateCardWithID(1);
+        GameObject _cardPrefabInstance = null;
+
+        switch (card.GetElement())
+        {
+            case "Intimidation":
+                _cardPrefabInstance = Instantiate(redCardNoEncounter, _deckContainerControllers[0].spawn.position, _deckContainerControllers[0].spawn.rotation, this.gameObject.transform);
+                break;
+            case "Sympathy":
+                _cardPrefabInstance = Instantiate(blueCardNoEncounter, _deckContainerControllers[0].spawn.position, _deckContainerControllers[0].spawn.rotation, this.gameObject.transform);
+                break;
+            case "Persuasion":
+                _cardPrefabInstance = Instantiate(greenCardNoEncounter, _deckContainerControllers[0].spawn.position, _deckContainerControllers[0].spawn.rotation, this.gameObject.transform);
+                break;
+            case "Preparation":
+                _cardPrefabInstance = Instantiate(greyCardNoEncounter, _deckContainerControllers[0].spawn.position, _deckContainerControllers[0].spawn.rotation, this.gameObject.transform);
+                break;
+        }
+
+        if (_cardPrefabInstance == null)
+        {
+            Debug.LogWarning("I cry evertim");
+        }
+
+        if (_cardPrefabInstance.GetComponent<NoEncounterCardPrefabController>() == null)
+        {
+            Debug.LogWarning("I cry evertim2");
+        }
+
+        NoEncounterCardPrefabController c = _cardPrefabInstance.GetComponent<NoEncounterCardPrefabController>();
+        card.SetAndInitializeNoEncounterFrontendController(c);
+        _deckContainerControllers[0].SetCard(card);
+
     }
 
 
