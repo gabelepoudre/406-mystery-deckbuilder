c9ac8d1c8d34d30593f20bbe36e0961a4b5598ab
Gabriel LePoudre <gabelepoudre@gmail.com>
2023-03-12 17:13:02 -0600

    More visible card stuff

--- mystery-deckbuilder/Assets/Scripts/World UI/DeckUIController.cs
+++ mystery-deckbuilder/Assets/Scripts/World UI/DeckUIController.cs
@@ -5,7 +5,25 @@ using UnityEngine.UI;
 
 public class DeckUIController : MonoBehaviour
 {
-    public GameObject[] deckSpawns;
-    public GameObject[] collectionSpawns;
-    
+    public GameObject[] deckContainers;
+    public GameObject[] collectionContainers;
+
+    public GameObject redCardNoEncounter;
+    public GameObject blueCardNoEncounter;
+    public GameObject greenCardNoEncounter;
+    public GameObject greyCardNoEncounter;
+
+    private List<Text> _deckQuantities = new();
+    private List<GameObject> _deckSpawns = new();
+
+    public void Start()
+    {
+        foreach(GameObject deckContainer in deckContainers)
+        {
+            _deckQuantities.Add(deckContainer.GetComponentInChildren<Text>());
+
+        }
+    }
+
+
 }
