8c39f529aeadfec2a24316698925f7faefaab8ba
Gabriel LePoudre <gabelepoudre@gmail.com>
2023-03-12 15:34:30 -0600

    deck ui script

---
+++ mystery-deckbuilder/Assets/Scripts/World UI/DeckUIController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckUIController : MonoBehaviour
+{
+    public GameObject[] deckSpawns;
+    public GameObject[] collectionSpawns;
+    
+}
