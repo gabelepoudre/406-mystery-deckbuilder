fea88ed30cd7d1730371aea1a1162070441bbcf8
Gabriel LePoudre <gabelepoudre@gmail.com>
2023-04-04 02:40:35 -0600

    Update EncounterImageController.cs

--- mystery-deckbuilder/Assets/Scripts/Encounter/EncounterImageController.cs
+++ mystery-deckbuilder/Assets/Scripts/Encounter/EncounterImageController.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class EncounterImageController : MonoBehaviour
 {
@@ -17,9 +18,22 @@ public class EncounterImageController : MonoBehaviour
     
     public void ChangeSize()
     {
-        // calculating the height of the encounter sprite, ensuring the width is always 600
-        float NewHeight = (NPCHeadshot.sprite.rect.height * scale) / NPCHeadshot.sprite.rect.width;
-        NPCHeadshot.rectTransform.sizeDelta = new Vector2(scale, NewHeight);
+        try
+        {
+            // calculating the height of the encounter sprite, ensuring the width is always 600
+            float NewHeight = (NPCHeadshot.sprite.rect.height * scale) / NPCHeadshot.sprite.rect.width;
+            NPCHeadshot.rectTransform.sizeDelta = new Vector2(scale, NewHeight);
+        }
+        catch (MissingReferenceException e)
+        {
+            e.Message.Contains("e");
+            NPCHeadshot.gameObject.SetActive(false);
+        }
+        catch (NullReferenceException e)
+        {
+            e.Message.Contains("e");
+            NPCHeadshot.gameObject.SetActive(false);
+        }
     }
 
     // Start is called before the first frame update
